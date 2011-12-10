f9200084e289d965ece9c741464a385521addadf
Moussa Tabcharani <moussa.tabcharani@gmail.com>
2011-12-10 14:14:00 -0500

    caption plugin has been completed V3 with fixes based on feedback from dseif

--- plugins/caption/popcorn.caption.js
+++ plugins/caption/popcorn.caption.js
@@ -5,28 +5,28 @@
   var i = 0,
       createDefaultContainer = function( context ) {
 
-        var ctxContainer = context.container = document.createElement( "div" ),
-            style = ctxContainer.style,
-            media = context.media;
+    var ctxContainer = context.container = document.createElement( "div" ),
+      style = ctxContainer.style,
+      media = context.media;
 
-        var updatePosition = function() {
-          var position = context.position();
-          // the video element must have height and width defined
-          style.width = media.offsetWidth + "px";
-		  
-		  style.top = position.top + "px";
-          style.left = position.left + "px";
+    var updatePosition = function() {
+      var position = context.position();
+      // the video element must have height and width defined
+      style.width = media.offsetWidth + "px";
 
-          setTimeout( updatePosition, 10 );
-        };
+      style.top = position.top + "px";
+      style.left = position.left + "px";
 
-        ctxContainer.id = Popcorn.guid();
-        style.position = "absolute";
+      setTimeout( updatePosition, 10 );
+    };
 
-        updatePosition();
+    ctxContainer.id = Popcorn.guid();
+    style.position = "absolute";
 
-        context.media.parentNode.appendChild( ctxContainer );
-      };
+    updatePosition();
+
+    context.media.parentNode.appendChild( ctxContainer );
+  };
 
   /**
    * Caption popcorn plug-in
@@ -35,146 +35,146 @@
    * Optional parameters are target, text, color and size.
    * Start is the time that you want this plug-in to execute
    * End is the time that you want this plug-in to stop executing
-   * PosX is the X position in pixels (top) 
+   * PosX is the X position in pixels (top)
    * Target is the id of the document element that the content is
    *  appended to, this target element must exist on the DOM
    * Text is the text of the caption you want to display.
    *
    * @param {Object} options
-   * 
+   *
    * Example:
-     var p = Popcorn('#video')
+     var p = Popcorn( "#video" )
         .caption({
           start:        5,                   // seconds, mandatory
           end:          15,                  // seconds, mandatory
-		  color:        'red',		         // optional
-		  size:         '20',		         // pixels, optional
-		  posx:			'0', 			     // pixels, left
-		  posy:			'0',				 // pixels, top
-		  shadow:		'black 2px 2px 6px', // optional
-		  weight:		'bold',				 // optional
-          text:         'Hellow world',      // optional
-          target:       'captiondiv',        // optional
+          color:        "red",               // optional
+          size:         "20",                // pixels, optional
+          posx:         "0",                 // pixels, left
+          posy:         "0",                 // pixels, top
+          shadow:       "black 2px 2px 6px", // optional
+          weight:       "bold",              // optional
+          text:         "Hellow world",      // optional
+          target:       "captiondiv",        // optional
         } )
    *
    */
 
   Popcorn.plugin( "caption" , {
-    
-      manifest: {
-        about: {
-          name: "Popcorn Caption Plugin",
-          version: "1.0",
-          author: "Moussa Tabcharani",
-          website: "http://moussa1.wordpress.com/"
+
+    manifest: {
+      about: {
+        name: "Popcorn Caption Plugin",
+        version: "1.0",
+        author: "Moussa Tabcharani",
+        website: "http://moussa1.wordpress.com/"
+      },
+      options: {
+        start: {
+          elem: "input",
+          type: "text",
+          label: "In"
         },
-        options: {
-          start: {
-            elem: "input",
-            type: "text",
-            label: "In"
-          },
-          end: {
-            elem: "input",
-            type: "text",
-            label: "Out"
-          },
-          target: "caption-container",
-          text: {
-            elem: "input",
-            type: "text",
-            label: "Text"
-          },
-		  color: {
-            elem: "input",
-            type: "text",
-            label: "Color"
-          },
-		  size: {
-            elem: "input",
-            type: "text",
-            label: "FontSize"
-          },
-		  posx: {
-            elem: "input",
-            type: "text",
-            label: "PositionX"
-          },
-		  posy: {
-            elem: "input",
-            type: "text",
-            label: "PositionY"
-          },
-     	  weight: {
-            elem: "input",
-            type: "text",
-            label: "FontWeight"
-          },
-		  shadow: {
-            elem: "input",
-            type: "text",
-            label: "TextShadow"
-          }
+        end: {
+          elem: "input",
+          type: "text",
+          label: "Out"
+        },
+        target: "caption-container",
+        text: {
+          elem: "input",
+          type: "text",
+          label: "Text"
+        },
+        color: {
+          elem: "input",
+          type: "text",
+          label: "Color"
+        },
+        size: {
+          elem: "input",
+          type: "text",
+          label: "FontSize"
+        },
+        posx: {
+          elem: "input",
+          type: "text",
+          label: "PositionX"
+        },
+        posy: {
+          elem: "input",
+          type: "text",
+          label: "PositionY"
+        },
+         weight: {
+          elem: "input",
+          type: "text",
+          label: "FontWeight"
+        },
+        shadow: {
+          elem: "input",
+          type: "text",
+          label: "TextShadow"
         }
-      },
-
-      _setup: function( options ) {
-
-        var newdiv = document.createElement( "div" );
-
-        newdiv.id = "caption-" + i++;
-        newdiv.style.display = "none";
-		newdiv.style.position = "absolute";
-		newdiv.style.color = options.color;
-		newdiv.style.fontSize = options.size + "px";
-		newdiv.style.textShadow = options.shadow;
-        newdiv.style.fontWeight = options.weight;
-
-		newdiv.style.top = ( options.posy > this.position().height ) ? this.position().height + "px" : options.posy + "px";
-		newdiv.style.left = ( options.posx > this.position().width ) ? this.position().width + "px" : options.posx + "px";
-				
-        // Creates a div for all captions to use
-        ( !this.container && ( !options.target || options.target === "caption-container" ) ) &&
-          createDefaultContainer( this );
-
-        // if a target is specified, use that
-        if ( options.target && options.target !== "caption-container" ) {
-          options.container = document.getElementById( options.target );
-        } else { 
-          // use shared default container
-          options.container = this.container;
-        }
-
-        document.getElementById( options.container.id ) && document.getElementById( options.container.id ).appendChild( newdiv );
-        options.innerContainer = newdiv;
-
-        options.showSubtitle = function() {
-          options.innerContainer.innerHTML = options.text;
-        };
-      },
-      /**
-       * @member caption 
-       * The start function will be executed when the currentTime 
-       * of the video  reaches the start time provided by the 
-       * options variable
-       */
-      start: function( event, options ){
-        options.innerContainer.style.display = "inline";
-        options.showSubtitle( options, options.text );
-      },
-      /**
-       * @member caption 
-       * The end function will be executed when the currentTime
-       * of the video  reaches the end time provided by the
-       * options variable
-       */
-      end: function( event, options ) {
-        options.innerContainer.style.display = "none";
-        options.innerContainer.innerHTML = "";
-      },
-
-      _teardown: function ( options ) {
-        options.container.removeChild( options.innerContainer );
       }
+    },
+
+    _setup: function( options ) {
+
+      var newdiv = document.createElement( "div" );
+
+      newdiv.id = "caption-" + i++;
+      newdiv.style.display = "none";
+      newdiv.style.position = "absolute";
+      newdiv.style.color = options.color;
+      newdiv.style.fontSize = options.size + "px";
+      newdiv.style.textShadow = options.shadow;
+      newdiv.style.fontWeight = options.weight;
+
+      newdiv.style.top = ( options.posy > this.position().height ) ? this.position().height + "px" : options.posy + "px";
+      newdiv.style.left = ( options.posx > this.position().width ) ? this.position().width + "px" : options.posx + "px";
+
+      // Creates a div for all captions to use
+      ( !this.container && ( !options.target || options.target === "caption-container" ) ) &&
+        createDefaultContainer( this );
+
+      // if a target is specified, use that
+      if ( options.target && options.target !== "caption-container" ) {
+        options.container = document.getElementById( options.target );
+      } else {
+        // use shared default container
+        options.container = this.container;
+      }
+
+      document.getElementById( options.container.id ) && document.getElementById( options.container.id ).appendChild( newdiv );
+      options.innerContainer = newdiv;
+
+      options.showSubtitle = function() {
+        options.innerContainer.innerHTML = options.text;
+      };
+    },
+    /**
+     * @member caption
+     * The start function will be executed when the currentTime
+     * of the video  reaches the start time provided by the
+     * options variable
+     */
+    start: function( event, options ){
+      options.innerContainer.style.display = "inline";
+      options.showSubtitle( options, options.text );
+    },
+    /**
+     * @member caption
+     * The end function will be executed when the currentTime
+     * of the video  reaches the end time provided by the
+     * options variable
+     */
+    end: function( event, options ) {
+      options.innerContainer.style.display = "none";
+      options.innerContainer.innerHTML = "";
+    },
+
+    _teardown: function ( options ) {
+      options.container.removeChild( options.innerContainer );
+    }
   });
 })( Popcorn );
--- plugins/caption/popcorn.caption.unit.js
+++ plugins/caption/popcorn.caption.unit.js
@@ -2,10 +2,10 @@ test("Popcorn Caption Plugin", function () {
 
   var popped = Popcorn( "#video" ),
       popped2 = Popcorn( "#video2" ),
-      expects = 6,
+      expects = 9,
       count = 0,
-	  posx,
-	  posy,
+      posx,
+      posy,
       captiondiv,
       caption2div;
 
@@ -18,78 +18,83 @@ test("Popcorn Caption Plugin", function () {
   }
 
   stop( 12000 );
-  
-  ok ( 'caption' in popped, "caption is a method of the popped instance" );
+
+  ok ( "caption" in popped, "caption is a method of the popped instance" );
   plus();
 
   popped.caption({
-      start: 0,
-      end: 2,
-      text: 'this is the first caption of 2011',
-	  color: 'red',
-	  size: 10,
-	  posx: 50,
-      posy: 30
-    } )
+    start: 0,
+    end: 2,
+    text: "instance 1 video 1 test",
+    color: "red",
+    size: 10,
+    posx: 50,
+    posy: 30
+  })
   .caption({
-      start: 2,
-      end: 4,
-      text: 'this is the second caption of 2011',
-	  color: 'white',
-	  size: 15,
-	  posx: 100,
-      posy: 50
-    } )
-	.volume( 0 )
-    .play();
+    start: 2,
+    end: 4,
+    text: "instance 2 video 1 test",
+    color: "white",
+    size: 15,
+    posx: 100,
+    posy: 50
+  })
+  .volume( 0 )
+  .play();
 
   captiondiv = popped.getTrackEvent( popped.getLastTrackEventId() ).container;
 
   popped.caption({
     start: 7,
     end: 9,
-    text: 'instance one test',
-	color: 'green',
-	size: 10,
-	posx: 100,
+    text: "instance 3 video 1 test",
+    color: "green",
+    size: 10,
+    posx: 100,
     posy: 50
   });
 
   popped2.caption({
-      start: 7,
-      end: 9,
-      text: 'instance two test',
-	  color: 'blue',
-	  size: 15,
-	  posx: 20,
-      posy: 20
-    })
-    .volume( 0 )
-    .play().pause();
+    start: 7,
+    end: 9,
+    text: "instance 1 video 2 test",
+    color: "blue",
+    size: 15,
+    posx: 20,
+    posy: 20
+  })
+  .caption({
+    start: 7,
+    end: 9,
+    text: "instance 2 video 2 test",
+    color: "red",
+    size: 15,
+    posx: 20,
+    posy: 40
+  })
+  .volume( 0 )
+  .play().pause();
 
   caption2div = popped2.getTrackEvent( popped2.getLastTrackEventId() ).container;
 
   popped.exec( 1, function() {
-
     popped.media.pause();
-    equals( captiondiv.children[ 0 ].innerHTML, "this is the first caption of 2011", "caption displaying correct information" );
+    equals( captiondiv.children[ 0 ].innerHTML, "instance 1 video 1 test", "caption displaying correct information" );
     plus();
 
-	ok( captiondiv.children[ 0 ].style.top === "30px", "posy for caption 1 is correct" );
-	plus();
+    ok( captiondiv.children[ 0 ].style.top === "30px", "posy for caption 1 is correct" );
+    plus();
 
     popped.media.play();
-
   });
 
   popped.exec( 4, function() {
-
     popped.media.pause();
     equals (captiondiv.children[ 1 ].innerHTML, "", "caption is clear" );
     plus();
 
     popped.media.play();
-
   });
 
   popped.exec( 8, function() {
@@ -98,25 +103,31 @@ test("Popcorn Caption Plugin", function () {
   });
 
   popped2.exec( 8, function() {
-
     popped2.pause();
 
-	//let's test to see if the posx for caption 4 for the second video is correct
+    //let's test to see if the posx for caption 4 for the second video is correct
     ok( caption2div.children[ 0 ].style.left === "20px", "posx for caption 4 is correct" );
     plus();
 
-	popped.media.play();
+    //let's test to see if the color for caption 4 is blue
+    ok( document.getElementById( "caption-3" ).style.color === "blue", "color for caption 4 is correct (blue)" );
+    plus();
 
+    ok( caption2div.children[ 0 ].innerHTML === "instance 1 video 2 test", "instance 1 for video 2 is correct at 8 seconds" );
+    plus();
+
+    ok( caption2div.children[ 1 ].innerHTML === "instance 2 video 2 test", "instance 2 for video 2 is correct at 8 seconds" );
+    plus();
+
+    popped.media.play();
   });
 
   popped.exec( 10, function() {
-
-	//let's test to see if the all the caption divs disappeared at 10 seconds for video 1
-	//the last caption for video 1 should end at 9 seconds
+    //let's test to see if the all the caption divs disappeared at 10 seconds for video 1
+    //the last caption for video 1 should end at 9 seconds
     ok( document.getElementById( "caption-0" ).style.display === "none" &&
         document.getElementById( "caption-1" ).style.display === "none" &&
         document.getElementById( "caption-2" ).style.display === "none", "All captions are no longer visible" );
     plus();
-
   });
 });
